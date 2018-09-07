72d0cbad4fc667a368a2c831f5fcca6312f9a354
陈鹏鹏 <chen_peng06@163.com>
2018-09-08 00:11:10 +0800

    init commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Thinking In Java第四版读书笔记
+
+## [一切都是对象][对象]
+
+
+---
+[对象]:src/main/java/chapter2/readme.md
---
+++ src/main/java/chapter2/FieldInit.java
@@ -0,0 +1,14 @@
+package chapter2;
+
+public class FieldInit {
+    int x;
+
+    public static void main(String[] args) {
+        // 局部变量(方法中)没有初始值，而类的成员变量是有初始值的
+        //下面的y没有初始化，会在编译器报错
+        //int y;
+        FieldInit baseType = new FieldInit();
+        System.out.println(baseType.x);
+       // System.out.println(y);
+    }
+}
